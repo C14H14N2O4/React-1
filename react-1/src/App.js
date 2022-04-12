b6bf6895c4f89efeecf0559dfc1e41677ec6b09a
GaussianSums <99705187+C14H14N2O4@users.noreply.github.com>
2022-04-11 21:47:24 -0700

    Clock and minor changes

--- react-1/src/App.js
+++ react-1/src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
+<<<<<<< Updated upstream
+=======
+import Clock from './Clock'
+import {Link} from "react-router-dom";
+>>>>>>> Stashed changes
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+<<<<<<< Updated upstream
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hey Howdy</p>
         <p>
@@ -18,6 +25,17 @@ function App() {
         >
           Learn React
         </a>
+=======
+        <nav 
+          style={{
+            borderBottom: "solid 1px",
+            paddingBottom: "1rem",
+          }}
+          >
+            <Link to="/about">About</Link>
+          </nav>
+        <Clock></Clock>
+>>>>>>> Stashed changes
       </header>
     </div>
   );
